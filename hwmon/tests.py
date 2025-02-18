7d4072ce280a22b965e11643a368b68463e526fb
Kwanghoon Son <kwangson@yahoo.com>
2025-02-18 23:19:47 +0900

    Add pyaudio
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- hwmon/tests.py
+++ hwmon/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+import pyaudio
 
-# Create your tests here.
+
+class SpeakerTestCase(TestCase):
+    def setUp(self):
+        self.p = pyaudio.PyAudio()
+
+    def tearDown(self):
+        self.p.terminate()
+
+    def test_speak_hello(self):
+        self.assertEqual(1 + 1, 2)
