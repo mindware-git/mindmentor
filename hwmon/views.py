674cc950cd9173ad9bc83cbfc6a6da43ad6260db
Kwanghoon Son <kwangson@yahoo.com>
2025-02-20 22:51:34 +0900

    Add hwmon mode
    Make hwmon and lectures, ml isolate.
    They needs interact each so there is no dependancy.
    Also use Django signal so simple approch.
    
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- hwmon/views.py
+++ hwmon/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 import os
 import pyaudio
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+MM_MODE = "idle"
 
 
 def index(request):
@@ -14,3 +18,22 @@ def index(request):
         "sound": info,
     }
     return render(request, "hwmon/index.html", context)
+
+
+@csrf_exempt
+def mode(request):
+    global MM_MODE
+
+    valid_modes = ["idle", "lecturer", "teaching_assistant"]
+
+    if request.method == "POST":
+        print("current mode " + MM_MODE)
+        mm_mode = request.POST.get("mode")
+
+        print("mm_mode is " + mm_mode)
+        MM_MODE = mm_mode
+        return JsonResponse({"status": "success"})
+    elif request.method == "GET":
+        print("current mode " + MM_MODE)
+        return JsonResponse({"current_mode": MM_MODE})
+    return JsonResponse({"status": "failed"}, status=400)
