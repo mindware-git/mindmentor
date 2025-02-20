2f4f7f9e9cd0aeeb6998939d0d14065105549789
Kwanghoon Son <kwangson@yahoo.com>
2025-02-20 14:50:49 +0900

    Add teacher page
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- lectures/views.py
+++ lectures/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+# from .models import Teacher, Subject, Course, Lecture
 
 
 def learners(request):
@@ -6,4 +10,18 @@ def learners(request):
 
 
 def teachers(request):
-    return HttpResponse("Hello, teachers.")
+    # teacher = get_object_or_404(Teacher, id=teacher_id)
+    # # Assuming you have a way to get the subject, course, and lecture
+    # subject = Subject.objects.first()  # Replace with actual logic
+    # course = Course.objects.filter(subject=subject).first()  # Replace with actual logic
+    # lecture = Lecture.objects.filter(course=course).first()  # Replace with actual logic
+
+    template = loader.get_template("lectures/teacher_detail.html")
+
+    context = {
+        "teacher": "Jone doe",
+        "subject": "Math",
+        "course": "2nd grade",
+        "lecture": "Add and subtraction",
+    }
+    return HttpResponse(template.render(context, request))
--- mm/settings.py
+++ mm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "hwmon",
+    "ml",
+    "lectures",
 ]
 
 MIDDLEWARE = [
