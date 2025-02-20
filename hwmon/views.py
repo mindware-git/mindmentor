3a6e9ef7dd817def2d4b572ad8eb897729bfdc09
Kwanghoon Son <kwangson@yahoo.com>
2025-02-20 16:05:02 +0900

    Add hwmon page
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- hwmon/views.py
+++ hwmon/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+import os
+import pyaudio
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the hwmon index.")
+    p = pyaudio.PyAudio()
+    info = []
+    for i in range(p.get_device_count()):
+        info.append(p.get_device_info_by_index(i))
+    context = {
+        "os": os.uname(),
+        "num_cameras": "? TODO",
+        "sound": info,
+    }
+    return render(request, "hwmon/index.html", context)
