a102264207c355c1583a90f50bd51cf6d90c6ced
Kwanghoon Son <kwangson@yahoo.com>
2025-02-21 09:52:35 +0900

    Add playing wav
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- hwmon/views.py
+++ hwmon/views.py
@@ -3,6 +3,7 @@ import os
 import pyaudio
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import wave
 
 MM_MODE = "idle"
 
@@ -33,11 +34,45 @@ def mode(request):
         print("mm_mode is " + mm_mode)
         if mm_mode == "teaching_assistant":
             print("listening")
+            play_audio("hwmon/res/react_sara.wav")
         elif mm_mode == "lecturer":
             print("let's start talking classs meterials")
+            play_audio("hwmon/res/lecture1.wav")
         MM_MODE = mm_mode
         return JsonResponse({"status": "success"})
     elif request.method == "GET":
         print("current mode " + MM_MODE)
         return JsonResponse({"current_mode": MM_MODE})
     return JsonResponse({"status": "failed"}, status=400)
+
+
+def play_audio(wav_file_path):
+
+    # Open the WAV file
+    wf = wave.open(wav_file_path, "rb")
+
+    # Create a PyAudio object
+    p = pyaudio.PyAudio()
+
+    # Open a stream to play the audio
+    stream = p.open(
+        format=p.get_format_from_width(wf.getsampwidth()),
+        channels=wf.getnchannels(),
+        rate=wf.getframerate(),
+        output=True,
+    )
+
+    # Read data in chunks
+    chunk_size = 1024
+    data = wf.readframes(chunk_size)
+
+    # Play the audio
+    while data:
+        stream.write(data)
+        data = wf.readframes(chunk_size)
+
+    # Stop and close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf.close()
