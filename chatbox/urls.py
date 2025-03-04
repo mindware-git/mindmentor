cf50988f8447b3d655dfb7ab95decdd10f27d63d
Kwanghoon Son <kwangson@yahoo.com>
2025-03-04 16:50:24 +0900

    Setup api and view
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- chatbox/urls.py
+++ chatbox/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("auth", views.auth),
+    path("api/v1/auth", views.auth),
+    path("api/v1/users/learner/ask", views.ask_question, name="ask_question"),
+    path("api/v1/users/teacher/start", views.start_lecture, name="start_lecture"),
+    path("api/v1/lectures", views.get_lecture, name="get_lecture"),
+    path("api/v1/robot/mode", views.mode),
     path("users/teacher", views.teachers),
     path("users/learner", views.learners),
-    path("users/learner/ask", views.ask_question, name="ask_question"),
-    path("users/teacher/start", views.start_lecture, name="start_lecture"),
-    path("lectures", views.get_lecture, name="get_lecture"),
     path("robot/status", views.robot_status),
-    path("robot/mode", views.mode),
 ]
