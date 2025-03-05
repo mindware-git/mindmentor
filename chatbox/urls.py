e399e4b51c73631791f53ee5b6154c18cf24eb5a
Kwanghoon Son <kwangson@yahoo.com>
2025-03-05 21:03:57 +0900

    Fix url robot mode
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- chatbox/urls.py
+++ chatbox/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("api/v1", include("chatbox.api_urls")),
+    path("api/v1/", include("chatbox.api_urls")),
     path("home", views.home),
     path("users/teacher", views.teachers),
     path("users/learner", views.learners),
