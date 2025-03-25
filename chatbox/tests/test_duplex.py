1bf269f32283a32dd34eb384b8c1f5b084fbfd32
Kwanghoon Son <kwangson@yahoo.com>
2025-03-26 07:43:51 +0900

    Init duplex
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

---
+++ chatbox/tests/test_duplex.py
@@ -0,0 +1,10 @@
+import unittest
+
+# Create function for question detection.
+
+# Create function for stoppable answering.
+
+
+class TestDuplex(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
